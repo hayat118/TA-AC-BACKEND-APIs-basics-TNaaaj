21845808654630a080e895a1049fce7c3238a972
ravi11o <ravi.suraj110@gmail.com>
2022-12-21 11:50:13 +0530

    assignment3 book

---
+++ block-BNaaej/sampleAPI-1/models/book.js
@@ -0,0 +1,14 @@
+var mongoose=require('mongoose')
+var Schema=mongoose.Schema;
+
+var bookSchema= new Schema({
+  name:{type:String},
+  title:{type:String},
+  description:{type:String},
+  page:{type:Number},
+  category:{type:Schema.Types.ObjectId, ref:"Category"},
+  comments:[{type:Schema.Types.ObjectId, ref:"Comment"}]
+},{timestamps:true});
+
+var Book=mongoose.model("Book",bookSchema);
+module.exports=Book;
